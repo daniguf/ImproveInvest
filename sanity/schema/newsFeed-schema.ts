3a4c0be120b4c3a6f8c79184c539a0d930213b62
Daniel Saleh <dasa0006@stud.ek.dk>
2025-11-12 15:55:10 +0100

    ci: implemented video file upload to content schemas

--- sanity/schema/newsFeed-schema.ts
+++ sanity/schema/newsFeed-schema.ts
@@ -1,39 +1,11 @@
-import { defineArrayMember, defineField, defineType } from "sanity";
+import { defineType } from "sanity";
+import { globalContentFields } from "./sharedFields";
 
 export const newsFeed = defineType({
   title: "News Feed",
   name: "newsFeed",
   type: "document",
-  fields: [
-    defineField({
-      title: "Name",
-      name: "name",
-      type: "string",
-    }),
-    defineField({
-      title: "Slug",
-      name: "slug",
-      type: "slug",
-      options: { source: "name" },
-    }),
-    defineField({
-      title: "Image",
-      name: "image",
-      type: "image",
-      fields: [{ title: "Alt", name: "alt", type: "string" }],
-    }),
-    defineField({
-      title: "URL",
-      name: "url",
-      type: "url",
-    }),
-    defineField({
-      name: "content",
-      title: "Content",
-      type: "array",
-      of: [defineArrayMember({ type: "block" })],
-    }),
-  ],
+  fields: globalContentFields,
 });
 
 export default newsFeed;
