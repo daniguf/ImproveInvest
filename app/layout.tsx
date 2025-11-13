6faaa7e34d6ff0b3c15de8e4ceaa2e90507affeb
Daniel Saleh <dasa0006@stud.ek.dk>
2025-11-13 19:06:47 +0100

    style: implemented brand color palettes and typography

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,11 @@
 import type { Metadata } from "next";
+import { Merriweather } from "next/font/google";
 import "./globals.css";
+
+const merriweather = Merriweather({
+  subsets: ["latin"],
+  weight: ["400", "700", "800"], // choose the weights you need
+});
 
 export const metadata: Metadata = {
   title: "Improve Invest",
@@ -8,11 +14,11 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
-    <html lang="en">
+    <html lang="en" className={merriweather.className}>
       <body>{children}</body>
     </html>
   );
