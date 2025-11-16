9b1f31ddd7b58eca968d33532208cea92173a593
dasa0006 <dasa0006@stud.ek.dk>
2025-11-16 02:36:07 +0100

    fix: proper mocks for navitems

--- components/ui/navItem/NavItem.mocks.ts
+++ components/ui/navItem/NavItem.mocks.ts
@@ -1,7 +1,10 @@
 import { INavItem } from "./NavItem";
 
 const base: INavItem = {
-  sampleTextProp: "Hello world!",
+  category: { id: 1, label: "Home", href: "/" },
+  handleOpen: () => {},
+  isOpen: false,
+  isAnyOpen: false,
 };
 
 export const mockNavItemProps = base;
